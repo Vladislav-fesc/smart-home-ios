da895129f06a1775b4f77abb27b9079d930e906c
Vlad <feschukovva@gmail.com>
2023-11-23 12:57:22 +0700

    init

---
+++ SmartHomeios/SmartHomeiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeiosApp.swift
+//  SmartHomeios
+//
+//  Created by Влад Фещуков   on 18.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SmartHomeiosUITests/SmartHomeiosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SmartHomeiosUITestsLaunchTests.swift
+//  SmartHomeiosUITests
+//
+//  Created by Влад Фещуков   on 18.11.2023.
+//
+
+import XCTest
+
+final class SmartHomeiosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
